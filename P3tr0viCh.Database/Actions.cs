f2190bcc4e015031c9275b79660118d13518b8b5
Константин Петрович <P3tr0viCh@users.noreply.github.com>
2025-12-11 17:00:29 +0500

    + Action. + Query.

---
+++ P3tr0viCh.Database/Actions.cs
@@ -0,0 +1,60 @@
+﻿using Dapper;
+using Dapper.Contrib.Extensions;
+using P3tr0viCh.Utils;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace P3tr0viCh.Database
+{
+    public static class Actions
+    {
+        public static async Task ListItemSaveAsync<T>(DbConnection connection, DbTransaction transaction, BaseId value) where T : BaseId
+        {
+            if (value.Id == Sql.NewId)
+            {
+                await connection.InsertAsync(value, transaction);
+            }
+            else
+            {
+                await connection.UpdateAsync(value, transaction);
+            }
+        }
+
+        public static async Task ListItemDeleteAsync<T>(DbConnection connection, DbTransaction transaction, BaseId value) where T : BaseId
+        {
+            await connection.DeleteAsync(value, transaction);
+        }
+
+        public static async Task<List<T>> ListLoadAsync<T>(DbConnection connection, Query query = null)
+        {
+            DebugWrite.Line(typeof(T).Name);
+
+            if (query == null)
+            {
+                query = new Query()
+                {
+                    table = Sql.TableName<T>(),
+                };
+            }
+
+            var sql = query.Select();
+
+            DebugWrite.Line(sql);
+
+            try
+            {
+                var list = await connection.QueryAsync<T>(sql);
+
+                return list.AsList();
+            }
+            catch (Exception e)
+            {
+                Sql.ExceptionAddQuery(e, sql);
+
+                throw;
+            }
+        }
+    }
+}
--- P3tr0viCh.Database/Sql.cs
+++ P3tr0viCh.Database/Sql.cs
@@ -4,47 +4,9 @@ using System.Linq;
 
 namespace P3tr0viCh.Database
 {
-    public static class Sql
+    public class Sql
     {
         public static long NewId = 0;
-
-        public class Query
-        {
-            public string fields = string.Empty;
-            public string table = string.Empty;
-            public string where = string.Empty;
-            public string group = string.Empty;
-            public string order = string.Empty;
-            public int limit = 0;
-            public int offset = 0;
-
-            public string Select()
-            {
-                if (fields.IsEmpty())
-                {
-                    fields = "*";
-                }
-
-                var sql = "SELECT " + fields;
-
-                sql = sql.JoinExcludeEmpty(" FROM ", table);
-                sql = sql.JoinExcludeEmpty(" WHERE ", where);
-                sql = sql.JoinExcludeEmpty(" GROUP BY ", group);
-                sql = sql.JoinExcludeEmpty(" ORDER BY ", order);
-
-                if (limit > 0)
-                {
-                    sql = sql.JoinExcludeEmpty(" LIMIT ", limit.ToString());
-                }
-
-                if (offset > 0)
-                {
-                    sql = sql.JoinExcludeEmpty(" OFFSET ", offset.ToString());
-                }
-
-                return sql;
-            }
-        }
 
         public static string TableName<T>()
         {
