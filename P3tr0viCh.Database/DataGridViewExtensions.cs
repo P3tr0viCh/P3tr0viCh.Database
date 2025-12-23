219786bbb92787660720fe7a1d6605e66d7a2e81
Константин Петрович <P3tr0viCh@users.noreply.github.com>
2025-12-23 12:35:23 +0500

    ~ DataGridViewExtensions.

--- P3tr0viCh.Database/DataGridViewExtensions.cs
+++ P3tr0viCh.Database/DataGridViewExtensions.cs
@@ -6,39 +6,74 @@ namespace P3tr0viCh.Database
 {
     public static class DataGridViewExtensions
     {
+        public static bool IsEmpty(this DataGridView dataGridView) => dataGridView.Rows.Count == 0;
+
+        public static DataGridViewRow Find(this DataGridView dataGridView, BaseId value)
+        {
+            if (value == null) return null;
+
+            if (dataGridView.IsEmpty()) return null;
+
+            foreach (var row in from DataGridViewRow row in dataGridView.Rows
+                                where (row.DataBoundItem as BaseId).Id == value.Id
+                                select row)
+            {
+                return row;
+            }
+
+            return null;
+        }
+
+        public static IEnumerable<DataGridViewRow> Find(this DataGridView dataGridView, IEnumerable<BaseId> values)
+        {
+            if (dataGridView.IsEmpty()) return Enumerable.Empty<DataGridViewRow>();
+
+            if (values?.Any() != true) return Enumerable.Empty<DataGridViewRow>();
+
+            var result = Enumerable.Empty<DataGridViewRow>();
+
+            foreach (var row in from value in values
+                                from row in
+                                    from DataGridViewRow row in dataGridView.Rows
+                                    where (row.DataBoundItem as BaseId).Id == value.Id
+                                    select row
+                                select row)
+            {
+                result = result.Append(row);
+            }
+
+            return result;
+        }
+
+        public static void SelectAndScroll(this DataGridViewRow row)
+        {
+            row.Selected = true;
+
+            if (!row.Displayed)
+            {
+                row.DataGridView.FirstDisplayedScrollingRowIndex = row.Index;
+            }
+        }
+
         public static void SetSelectedRows(this DataGridView dataGridView, IEnumerable<BaseId> values)
         {
             dataGridView.ClearSelection();
 
-            foreach (var value in values)
+            var rows = dataGridView.Find(values);
+
+            foreach (var row in rows)
             {
-                foreach (var row in from DataGridViewRow row in dataGridView.Rows
-                                    where (row.DataBoundItem as BaseId).Id == value.Id
-                                    select row)
-                {
-                    row.Selected = true;
-
-                    dataGridView.FirstDisplayedScrollingRowIndex = row.Index;
-
-                    break;
-                }
+                row.Selected = true;
             }
+
+            rows.FirstOrDefault()?.SelectAndScroll();
         }
 
         public static void SetSelectedRows(this DataGridView dataGridView, BaseId value)
         {
             dataGridView.ClearSelection();
 
-            foreach (var row in from DataGridViewRow row in dataGridView.Rows
-                                where (row.DataBoundItem as BaseId).Id == value.Id
-                                select row)
-            {
-                row.Selected = true;
-
-                dataGridView.FirstDisplayedScrollingRowIndex = row.Index;
-
-                break;
-            }
+            dataGridView.Find(value)?.SelectAndScroll();
         }
     }
 }
